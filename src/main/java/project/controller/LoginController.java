261fe626bbb86cad73e1e761b60a8ca30a5fca38
svava <svavahb@gmail.com>
2017-03-28 22:12:19 +0000

    test

--- src/main/java/project/controller/LoginController.java
+++ src/main/java/project/controller/LoginController.java
@@ -50,6 +50,7 @@ public class LoginController {
     // Post method for logging in user
     @PostMapping(value="/login", consumes = MediaType.APPLICATION_JSON_VALUE)
     public boolean LogInPost(@RequestBody User LogIn) {
+        System.out.println(LogIn);
         // Attempt login, return the result
         return securityService.autologin(LogIn.getUsername(), LogIn.getPassword());
     }
