81435d8da4adff297c2922b6d39a7d2905f446ef
svava <svavahb@gmail.com>
2017-03-28 22:23:43 +0000

    breyta autologin smá

--- src/main/java/project/service/SecurityService.java
+++ src/main/java/project/service/SecurityService.java
@@ -5,6 +5,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import project.persistence.entities.User;
+import project.persistence.repositories.Repository;
 
 /**
  * Created by Svava on 16.11.16.
@@ -14,19 +16,20 @@ public class SecurityService {
 
     // Used for checking password
     PasswordEncoder pwEncoder = new BCryptPasswordEncoder();
+    Repository repository = new Repository();
 
     private UserDetailsService userDetailsService = new UserDetailsServiceImpl();
 
     // Log in user
     public boolean autologin(String username, String password) {
         // Check if username already exists
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if(userDetails.getUsername().equals("NotFound")) {
+        User user = repository.findUsersByName(username);
+        if(user.getUsername().isEmpty()) {
             return false;
         }
 
         // Check manually whether password is correct
-        if (pwEncoder.matches(password, userDetails.getPassword())) {
+        if (pwEncoder.matches(password, user.getPassword())) {
             return true;
         }
         return false;
